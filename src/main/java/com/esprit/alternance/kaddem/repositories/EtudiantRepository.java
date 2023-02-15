1e724e0b40deb040637fec5490dc634079c73823
SirineZn <sirine.znaidi@esprit.tn>
2023-02-15 09:39:25 +0100

    Add keywords in EtudiantRepository

--- src/main/java/com/esprit/alternance/kaddem/repositories/EtudiantRepository.java
+++ src/main/java/com/esprit/alternance/kaddem/repositories/EtudiantRepository.java
@@ -2,7 +2,13 @@ package com.esprit.alternance.kaddem.repositories;
 
 import com.esprit.alternance.kaddem.entities.Equipe;
 import com.esprit.alternance.kaddem.entities.Etudiant;
+import com.esprit.alternance.kaddem.entities.Niveau;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EtudiantRepository extends JpaRepository<Etudiant, Integer> {
+    List<Etudiant> findByDepartmentNomDepart(String nomDepart);
+    //List<Etudiant> findByDepartmentIdDepart(String idDepart);
+    List<Etudiant> findByEquipeNiveau(Niveau nv);
 }
